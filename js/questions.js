07e841a7c24c717e7a8a6b1f3746d9d0c8d6dbc0
araldwin <araldwin@gmail.com>
2022-08-30 16:58:56 +0000

    add function to the quiz box, and next button

---
+++ js/questions.js
@@ -0,0 +1,94 @@
+//creating an array and passing the number, questions, options, and answer
+let questions = [
+    {
+        numb: 1,
+        question: "The Great 'what' of London occurred in 1666?",
+        answer: "Fire",
+        options: [
+            "Fire",
+            "Flood"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What was Queen Mary of England's nickname?",
+        answer: "Bloody",
+        options: [
+            "Virgin",
+            "Bloody"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Elizabeth I of England was known as the 'what' Queen?",
+        answer: "Virgin",
+        options: [
+            "Virgin",
+            "Bloody"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What was the relationship between Mary and Elizabeth of England?",
+        answer: "Half-Sisters",
+        options: [
+            "Half-Sisters",
+            "Cousins"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What was Marie Curie most famous for being?",
+        answer: "Scientist",
+        options: [
+            "Scientist",
+            "Actress"
+        ]
+    },
+    {
+        numb: 6,
+        question: "The Aztecs were conquered by which European country?",
+        answer: "Spain",
+        options: [
+            "Portugal",
+            "Spain"
+        ]
+    },
+    {
+        numb: 7,
+        question: "The Aztecs lived primarily in the region known as what today?",
+        answer: "Mexico",
+        options: [
+            "Cuba",
+            "Mexico"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What was New York's original name?",
+        answer: "New Amsterdam",
+        options: [
+            "New Amsterdam",
+            "The Big Apple"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which energy source was important for the advances of the Industrial Revolution?",
+        answer: "Coal",
+        options: [
+            "Coal",
+            "Gas"
+        ]
+    },
+    {
+        numb: 10,
+        question: "The Shoguns ruled over which country?",
+        answer: "Japan",
+        options: [
+            "China",
+            "Japan"
+        ]
+    },
+
+];
--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
 const titleBox_rmv = document.querySelector(".title_box");
 const quiz_box = document.querySelector(".quiz_box");
+const option_list = document.querySelector(".option_list");
 
 //If Start Quiz Button Clicked
 start_btn.onclick = ()=>{
@@ -22,4 +23,55 @@ exit_btn.onclick = ()=>{
 continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide the info box
     quiz_box.classList.add("activeQuiz"); //Show the Quiz box
+    showQuestion(0);
+    queCounter(1);
 };
+
+let que_count = 0;
+let que_numb = 1;
+
+const next_btn = quiz_box.querySelector(".next_btn");
+
+// If Next Button Clicked
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){
+        que_count++;
+        que_numb++;
+        showQuestion(que_count);
+        queCounter(que_numb);
+    }else{
+        console.log("Questions completed");
+    }
+};
+
+//getting questions and options from array
+function showQuestion(index){
+    const que_text = document.querySelector(".que_text");
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'+'<div class="option"><span>'+ questions[index].options[1] +'</span></div>';
+    que_text.innerHTML = que_tag; 
+    option_list.innerHTML = option_tag;
+
+    const option = option_list.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+function optionSelected(answer){
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    if(userAns == correctAns){
+        answer.classList.add("correct");
+        console.log("Answer is Correct");
+    }else{
+        answer.classList.add("incorrect");
+        console.log("Answer is Wrong");
+    }
+}
+
+function queCounter(index){
+    const bottom_ques_counter = quiz_box.querySelector(".total_que");
+    let totalQuesCountTag = '<span><p>'+ index +'</p>of<p>'+ questions.length +'</p>Questions</span>';
+    bottom_ques_counter.innerHTML = totalQuesCountTag;
+}
